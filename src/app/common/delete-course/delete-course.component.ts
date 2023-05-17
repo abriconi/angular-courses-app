c1b7a7d655dee8f8221a9cb9e939f4db2eafef1f
Alyona Gepting <abriconi@gmail.com>
2023-05-17 14:00:10 +0300

    Created app structure

---
+++ src/app/common/delete-course/delete-course.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-course',
+  templateUrl: './delete-course.component.html',
+  styleUrls: ['./delete-course.component.scss']
+})
+export class DeleteCourseComponent {
+
+}
