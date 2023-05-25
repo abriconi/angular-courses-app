187a0e82fd63f5fc88643b933936304aeabb51f7
Alyona Gepting <abriconi@gmail.com>
2023-05-25 09:43:03 +0300

    Set courses array property inside ngOnInit hook, added to ngFor trackBy

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,4 +1,4 @@
-import { Component, } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { coursesMockedData } from 'src/app/utilus/global.constans';
 import { Course } from 'src/app/utilus/global.moduls';
 
@@ -7,11 +7,19 @@ import { Course } from 'src/app/utilus/global.moduls';
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
-  courses:Course[] = coursesMockedData;
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [];
+
+  trackCourseById(_index: number, course: Course): string {
+    return course.id;
+  }
+
+  ngOnInit(): void {
+    this.courses = coursesMockedData;
+    console.log('onInit');
+  }
 
   handleClick():void {
     console.log('Button "Load more" cliked');
   }
-
 }
