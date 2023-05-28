5eb8e2636c620a4af40daf6870db7c141b828661
Alyona Gepting <abriconi@gmail.com>
2023-05-28 15:22:05 +0300

    Created unit tests for Courses, Section & SearchForm components

--- src/app/components/courses/section/section.component.spec.ts
+++ src/app/components/courses/section/section.component.spec.ts
@@ -1,23 +1,33 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
-// import { SectionComponent } from './section.component';
+import { SectionComponent } from './section.component';
+import { SharedModule } from '../../../shared/shared.module';
 
-// describe('SectionComponent', () => {
-//   let component: SectionComponent;
-//   let fixture: ComponentFixture<SectionComponent>;
+describe('SectionComponent', () => {
+  let component: SectionComponent;
+  let fixture: ComponentFixture<SectionComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ SectionComponent ]
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SectionComponent ],
+      imports: [ReactiveFormsModule, SharedModule]
+    })
+    .compileComponents();
 
-//     fixture = TestBed.createComponent(SectionComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(SectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  
+  it('should log the search value when handleSearch is called', () => {
+    const consoleSpy = spyOn(console, 'log');
+    const searchValue = 'example search';
+    component.handleSearch(searchValue);
+    expect(consoleSpy).toHaveBeenCalledWith(searchValue);
+  });
+});
--- src/app/shared/search-form/search-form.component.spec.ts
+++ src/app/shared/search-form/search-form.component.spec.ts
@@ -1,23 +1,56 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
-// import { SearchFormComponent } from './search-form.component';
+import { SearchFormComponent } from './search-form.component';
+import { ButtonComponent } from '../button/button.component';
 
-// describe('SearchFormComponent', () => {
-//   let component: SearchFormComponent;
-//   let fixture: ComponentFixture<SearchFormComponent>;
+describe('SearchFormComponent', () => {
+  let component: SearchFormComponent;
+  let fixture: ComponentFixture<SearchFormComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ SearchFormComponent ]
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SearchFormComponent, ButtonComponent ],
+      imports: [ReactiveFormsModule],
+    })
+    .compileComponents();
 
-//     fixture = TestBed.createComponent(SearchFormComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(SearchFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should emit search event with searchForm value when searchForm is valid', () => {
+    spyOn(component.search, 'emit');
+    component.searchForm.patchValue({ searchText: 'example' });
+    component.handleSearch();
+    expect(component.search.emit).toHaveBeenCalledWith({ searchText: 'example' });
+  });
+
+  // it('should not emit search event when searchForm is invalid', () => {
+  //   spyOn(component.search, 'emit');
+  //   component.searchForm.patchValue({ searchText: '' });
+  //   component.handleSearch();
+  //   fixture.detectChanges();
+  //   expect(component.search.emit).not.toHaveBeenCalled();
+  // });
+  // it('should not emit search event when searchForm is invalid', () => {
+  //   spyOn(component.search, 'emit');
+  //   component.searchForm.patchValue({ searchText: '' });
+  //   component.searchForm.markAllAsTouched(); // Trigger form validation
+  //   component.handleSearch();
+  //   expect(component.search.emit).not.toHaveBeenCalled();
+  // });
+  // it('should not emit search event when searchForm is invalid', () => {
+  //   spyOn(component.search, 'emit');
+  //   component.searchForm.patchValue({ searchText: '' });
+  //   component.searchForm.controls.searchText.updateValueAndValidity(); // Trigger form control validation
+  //   component.handleSearch();
+  //   expect(component.search.emit).not.toHaveBeenCalled();
+  // });
+
+});
