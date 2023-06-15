5b7aeb3746111c8bd108cf100227e454f1f78431
Alyona Gepting <abriconi@gmail.com>
2023-06-15 15:19:57 +0300

    Added unit tests to ConfirmationModal component

--- src/app/shared/components/confirmation-modal/confirmation-modal.component.spec.ts
+++ src/app/shared/components/confirmation-modal/confirmation-modal.component.spec.ts
@@ -1,23 +1,70 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ButtonComponent } from '../button/button.component';
+import { IconComponent } from '../icon/icon.component';
+import { By } from '@angular/platform-browser';
 
-// import { ConfirmationModalComponent } from './confirmation-modal.component';
+import { ConfirmationModalComponent } from './confirmation-modal.component';
 
-// describe('ConfirmationModalComponent', () => {
-//   let component: ConfirmationModalComponent;
-//   let fixture: ComponentFixture<ConfirmationModalComponent>;
+describe('ConfirmationModalComponent', () => {
+  let component: ConfirmationModalComponent;
+  let fixture: ComponentFixture<ConfirmationModalComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ ConfirmationModalComponent ]
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ConfirmationModalComponent, ButtonComponent, IconComponent ]
+    })
+    .compileComponents();
 
-//     fixture = TestBed.createComponent(ConfirmationModalComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(ConfirmationModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should emit closeModal event when onCloseModal is called', () => {
+    const closeModalSpy = spyOn(component.closeModal, 'emit');
+    component.onCloseModal();
+    expect(closeModalSpy).toHaveBeenCalled();
+  });
+
+
+  it('should emit closeModal and handleDelete events when onConfirm is called', () => {
+    const closeModalSpy = spyOn(component.closeModal, 'emit');
+    const handleDeleteSpy = spyOn(component.handleDelete, 'emit');
+    component.onConfirm();
+    expect(closeModalSpy).toHaveBeenCalled();
+    expect(handleDeleteSpy).toHaveBeenCalled();
+  });
+
+  it('should render course name in the modal description', () => {
+    const courseName = 'Mathematics';
+    component.courseName = courseName;
+    fixture.detectChanges();
+    const modalDescription = fixture.debugElement.query(By.css('.modalDescription')).nativeElement;
+    expect(modalDescription.textContent).toContain(courseName);
+  });
+
+  it('should trigger onCloseModal when close button is clicked', () => {
+    const closeButton = fixture.debugElement.query(By.css('.btnWrapper app-button'));
+    const onCloseModalSpy = spyOn(component, 'onCloseModal');
+    closeButton.triggerEventHandler('click', null);
+    expect(onCloseModalSpy).toHaveBeenCalled();
+  });
+
+  it('should trigger onCloseModal when cancel button is clicked', () => {
+    const cancelButton = fixture.debugElement.query(By.css('.buttonWrapper app-button:first-child'));
+    const onCloseModalSpy = spyOn(component, 'onCloseModal');
+    cancelButton.triggerEventHandler('click', null);
+    expect(onCloseModalSpy).toHaveBeenCalled();
+  });
+
+  it('should trigger onConfirm when delete button is clicked', () => {
+    const deleteButton = fixture.debugElement.query(By.css('.buttonWrapper app-button:last-child'));
+    const onConfirmSpy = spyOn(component, 'onConfirm');
+    deleteButton.triggerEventHandler('click', null);
+    expect(onConfirmSpy).toHaveBeenCalled();
+  });
+});
