36d0960a5eea13cf222f5802ab588dbc9583ef4e
Alyona Gepting <abriconi@gmail.com>
2023-06-16 19:49:38 +0300

    Added unit tests for Textaarea component

--- src/app/shared/components/textarea/textarea.component.spec.ts
+++ src/app/shared/components/textarea/textarea.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { TextareaComponent } from './textarea.component';
 
 describe('TextareaComponent', () => {
@@ -8,16 +8,25 @@ describe('TextareaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
       declarations: [ TextareaComponent ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(TextareaComponent);
     component = fixture.componentInstance;
+    component.control = new FormControl();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should bind the control to the formControl textarea', () => {
+    const formControl = new FormControl();
+    component.control = formControl;
+    fixture.detectChanges();
+    expect(component.control).toBe(formControl);
   });
 });
